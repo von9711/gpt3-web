9c25c3827c3d1b955296c770a56d44c0ee2d5728
Surya Pratap <62811612+von9711@users.noreply.github.com>
2022-05-31 00:32:46 +0530

    update Article component and addd stylesheet

--- src/components/article/Article.tsx
+++ src/components/article/Article.tsx
@@ -1,7 +1,27 @@
 import React, { ReactElement } from "react";
+import "./article.css";
 
-const Article = (): ReactElement => {
-  return <div>Article</div>;
+type ArticleProps = {
+  imageUrl: string;
+  date: string;
+  title: string;
+};
+
+const Article = ({ imageUrl, date, title }: ArticleProps): ReactElement => {
+  return (
+    <div className="gpt3__blog-container_article">
+      <div className="gpt3__blog-container_article-image">
+        <img src={imageUrl} alt="blog-thumbnail" />
+      </div>
+      <div className="gpt3__blog-container_article-content">
+        <div>
+          <p>{date}</p>
+          <h3>{title}</h3>
+        </div>
+        <p>Read Full article</p>
+      </div>
+    </div>
+  );
 };
 
 export default Article;
