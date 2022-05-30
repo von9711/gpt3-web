18961be3e7347ead4fdf463a3c901228fca37e1f
Surya Pratap <62811612+von9711@users.noreply.github.com>
2022-05-31 00:57:59 +0530

    update Footer component and add footer.css stylesheet

--- src/containers/footer/Footer.tsx
+++ src/containers/footer/Footer.tsx
@@ -1,7 +1,49 @@
 import React, { ReactElement } from "react";
+import gpt3Logo from "../../assets/logo.svg";
+import "./footer.css";
 
 const Footer = (): ReactElement => {
-  return <div>Footer</div>;
+  return (
+    <div className="gpt3__footer section__padding" id="footer">
+      <div className="gpt3__footer-heading">
+        <h1 className="gradient__text">
+          Do you want to step in to the future before others
+        </h1>
+      </div>
+      <div className="gpt3__footer-btn">
+        <p>Request Early Access</p>
+      </div>
+
+      <div className="gpt3__footer-links">
+        <div className="gpt3__footer-links_logo">
+          <img src={gpt3Logo} alt="logo" />
+        </div>
+        <div className="gpt3__footer-links_div">
+          <h4>Links</h4>
+          <p>Overons</p>
+          <p>Social Media</p>
+          <p>Counters</p>
+          <p>Contact</p>
+        </div>
+        <div className="gpt3__footer-links_div">
+          <h4>Company</h4>
+          <p>Terms & Conditions </p>
+          <p>Privacy Policy</p>
+          <p>Contact</p>
+        </div>
+        <div className="gpt3__footer-links_div">
+          <h4>Get in touch</h4>
+          <p>India</p>
+          <p>085-132567</p>
+          <p>von9711-github</p>
+        </div>
+      </div>
+
+      <div className="gpt3__footer-copyright">
+        <p>@2022 GPT-3. All rights reserved.</p>
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
